e72afbe53d83fd9afec13abcf0751697989604cd
Saivarun Illendula <s530464@nwmissouri.edu>
2018-06-21 01:47:23 -0500

    dfsfdad

---
+++ config/database.config.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+module.exports = mongoose.connect(' mongodb+srv://admin:admin@cluster0-sow0w.mongodb.net/flightreservation?retryWrites=true')
+ .then(() => {
+ console.log("DB connected")
+ }, err => {
+ console.log("DB error")
+ }
+ );
